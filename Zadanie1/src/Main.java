326051eb374372a6f297f853f84d869477932f55
l0858 <l0858@pjwstk.edu.pl>
2024-02-22 15:30:05 +0100

    jaja

--- Zadanie1/src/Main.java
+++ Zadanie1/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.lang.Math;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args){
-        int liczba  = 146;
-        generateTable();
+        int liczba  = 14676;
+        //generateTable();
+        cyfry(liczba);
     }
     public static int[][] generateTable(){
         int rows = 100;
@@ -20,7 +25,7 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < rows; i++) {
+       for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 System.out.print(randomArray[i][j] + "\t");
             }
@@ -30,7 +35,21 @@ public class Main {
         return randomArray;
     }
 
-    public int srednia(int[][] generateTable, int liczba){
+    public static ArrayList<Integer> cyfry(int liczba) {
+        ArrayList<Integer> liczby = new ArrayList<>();
+        for (int i = String.valueOf(liczba).length()-1; i > -1; i-- ){
+            int tmp = (int) Math.pow(10, i);
+            int reszta = liczba % tmp;
+            int digit = (liczba - reszta) / tmp;
+            liczba = reszta;
+            liczby.add(digit);
+        }
+        for (Integer integer : liczby) {
+            System.out.print(integer);
+        }
+        return liczby;
+    }
+   public int srednia(int[][] generateTable, int liczba){
     return 1;
     }
 }
