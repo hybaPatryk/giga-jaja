0a2b88916f060d34afc3d43dd5eb7017f145a2c4
l0858 <l0858@pjwstk.edu.pl>
2024-02-22 11:23:36 +0100

    jajo

--- Zadanie1/src/Main.java
+++ Zadanie1/src/Main.java
@@ -1,7 +1,36 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args){
+        int liczba  = 146;
+        generateTable();
+    }
+    public static int[][] generateTable(){
+        int rows = 100;
+        int columns = 10;
+
+        int[][] randomArray = new int[rows][columns];
+
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                int randomNumber = random.nextInt(99) + 1;
+                randomArray[i][j] = randomNumber;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(randomArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        return randomArray;
+    }
+
+    public int srednia(int[][] generateTable, int liczba){
+    return 1;
     }
 }
